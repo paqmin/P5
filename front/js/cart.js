0b75557a60d62fee968ee6314cc6fac261a0a55e
Frizon Mahalia <mahalia.frizon@gmail.com>
2022-12-12 11:19:49 +0100

    etape 8-affichage panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,16 +3,36 @@
 let panierActuel= localStorage.getItem("Panier");
 let Panier = JSON.parse(panierActuel);
 console.table(Panier);
-
+//recupération de la balise CSS pour l'affichage
+let carteArticle = document.getElementById("cart__items");
 //recuperation de ts les éléments nécessaires à l'affichage
 Panier.forEach(element => {
   const urlCanap = `http://localhost:3000/api/products/${element.id}`;
   fetch(urlCanap)
   .then(response => response.json())
   .then((data) => {
-    infoCanap = data;
-    console.log(data);
-    console.log(infoCanap.price)
+    infoJson = data;
+    carteArticle.innerHTML += `<article class="cart__item" data-id="${element.id}" data-color="${element.couleur}">
+    <div class="cart__item__img">
+    <img src="${infoJson.imageUrl}" alt="${infoJson.altTxt}">
+    </div>
+    <div class="cart__item__content">
+      <div class="cart__item__content__description">
+        <h2>${infoJson.name}</h2>
+        <p>${element.couleur}</p>
+        <p>${infoJson.price}€</p>
+      </div>
+      <div class="cart__item__content__settings">
+        <div class="cart__item__content__settings__quantity">
+          <p>Qté : ${element.quantite}</p>
+          <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${element.quantite}">
+        </div>
+        <div class="cart__item__content__settings__delete">
+          <p class="deleteItem">Supprimer</p>
+        </div>
+      </div>
+    </div>
+  </article>`;
   });
   
 });
